ddaa30537361dc3ab8af7cf7412d142f206a4a6f
fruitdude <stevenrogge95@gmail.com>
2021-02-10 14:32:46 +0100

    added files

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,7 @@
+extends KinematicBody2D
+class_name Actor
+
+export var speed: = Vector2(300.0, 1000)
+export var gravity: = 3000.0
+var _velocity: = Vector2.ZERO
+
---
+++ src/Objects/Coin.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+export var score: = 100
+
+
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	picked()
+	
+
+func picked() -> void:
+	anim_player.play("fade_out")
+	PlayerData.score += score
